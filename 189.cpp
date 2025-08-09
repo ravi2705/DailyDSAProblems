144865cb680ac25c6d10b03ac67379cdd96902c3
Ravi Vishwakarma <ravivrkv0@gmail.com>
2025-08-09 22:24:25 +0530

    Q-2 Done

---
+++ 189.cpp
@@ -0,0 +1,31 @@
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+
+        // Reverse the whole array
+        reverse(nums.begin(), nums.end());
+        // Reverse first k elements
+        reverse(nums.begin(), nums.begin() + k);
+        // Reverse the remaining elements
+        reverse(nums.begin() + k, nums.end());
+    }
+};
